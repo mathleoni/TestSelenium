81efc4ebf58dd0f027feb828dbf5aead2ec3f4eb
mathleoni <42352024+mathleoni@users.noreply.github.com>
2018-09-11 15:32:09 -0300

    Adiciona a master: clicar próx. pg, esperar resultados e imprimir seus dados (link, texto definição) (#1)
    *Vai para a próxima página
    *Adicionada ação de esperar página carregar (espera resultados);
    *Imprime link dos resultados e texto definição;

--- TesteSel.2/src/TesteSel/Teste_Sel.java
+++ TesteSel.2/src/TesteSel/Teste_Sel.java
@@ -5,8 +5,11 @@
  */
 package TesteSel;
 
+import java.util.List;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 /**
@@ -22,17 +25,31 @@ public class Teste_Sel {
     public static void teste() throws InterruptedException{
         //System.setProperty("webdriver.chrome.driver", "/home/leoni/Desktop");
         WebDriver driver = new ChromeDriver();
-        driver.get("http://www.google.com/xhtml");
-        Thread.sleep(5000);  // Let the user actually see something!
+        driver.get("https://www.google.com/?gl=us&hl=en&pws=0&gws_rd=cr");// Google US
+        Thread.sleep(5000);  // Tempo ms
         WebElement searchBox = driver.findElement(By.name("q"));
-        searchBox.sendKeys("teste");
+        searchBox.sendKeys("Lucy Rose Wiki");
         searchBox.submit();
-        Thread.sleep(5000);  // Let the user actually see something!
+        Thread.sleep(5000);  
         WebElement element = driver.findElement(By.className("rc"));
         String text = element.getText();
         System.out.println(text);
-         
-   
+        driver.findElement(By.linkText("Next")).click();
+        //espera a página carregar resultados
+        WebElement results =  (new WebDriverWait(driver, 10)).
+                until(ExpectedConditions.presenceOfElementLocated(By.id("resultStats")));
+     
+        List<WebElement> findElements = driver.findElements(By.xpath("//*[@id='rso']//h3/a"));
+
+        // links que aparecem no resultado da busca (após next pg)
+        for (WebElement webElement : findElements) {
+            System.out.println(webElement.getAttribute("href"));
+            WebElement elemento2 = driver.findElement(By.className("rc"));
+            String textoDef = elemento2.getText();
+            System.out.println(textoDef);
+            System.out.println("++++++++++++++\n");
+        }
+
     }
     public static void main(String[] args) throws InterruptedException{
         teste();
