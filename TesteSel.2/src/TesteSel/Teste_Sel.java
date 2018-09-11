d29119285f4906a2bdb5c8196dc14f1ccc080272
leoni <leoni@flebilis>
2018-09-11 15:28:39 -0300

    *Vai para a próxima página; *Adicionada ação de esperar página carregar (espera resultados); *Imprime link dos resultados e texto definição

--- TesteSel.2/src/TesteSel/Teste_Sel.java
+++ TesteSel.2/src/TesteSel/Teste_Sel.java
@@ -5,8 +5,11 @@
  */
 package TesteSel;
 
+import java.util.List;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 /**
@@ -32,10 +35,21 @@ public class Teste_Sel {
         String text = element.getText();
         System.out.println(text);
         driver.findElement(By.linkText("Next")).click();
-        
-        
-         
-   
+        //espera a página carregar resultados
+        WebElement results =  (new WebDriverWait(driver, 10)).
+                until(ExpectedConditions.presenceOfElementLocated(By.id("resultStats")));
+     
+        List<WebElement> findElements = driver.findElements(By.xpath("//*[@id='rso']//h3/a"));
+
+        // links que aparecem no resultado da busca (após next pg)
+        for (WebElement webElement : findElements) {
+            System.out.println(webElement.getAttribute("href"));
+            WebElement elemento2 = driver.findElement(By.className("rc"));
+            String textoDef = elemento2.getText();
+            System.out.println(textoDef);
+            System.out.println("++++++++++++++\n");
+        }
+
     }
     public static void main(String[] args) throws InterruptedException{
         teste();
